b9035b0d95730dd4d6a83449e45e3cb3808df616
Jordan Pace <jordanpace95@gmail.com>
2023-04-27 17:49:50 -0600

    adds asset caching and injectmanifest

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -3,9 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
-
 module.exports = () => {
   return {
     mode: 'development',
@@ -13,17 +10,25 @@ module.exports = () => {
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
+    
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
         title: 'JATE'
       }),
 
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
       new GenerateSW(),
+
       new WebpackPwaManifest({
         name: 'Jate Progressive Web App',
         short_name: 'JatePWA',
