b2b1975c20a513b38a960073f7de9be8953503e1
Jordan Pace <jordanpace95@gmail.com>
2023-04-28 00:39:24 -0600

    had to move some files around

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,6 +3,9 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
 module.exports = () => {
   return {
     mode: 'development',
@@ -10,12 +13,10 @@ module.exports = () => {
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
-    
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-
     plugins: [
       new HtmlWebpackPlugin({
         template: './index.html',
@@ -27,11 +28,10 @@ module.exports = () => {
         swDest: 'src-sw.js',
       }),
 
-      // new GenerateSW(),
-
       new WebpackPwaManifest({
         name: 'Jate Progressive Web App',
         fingerprints: false,
+        inject: true,
         short_name: 'JatePWA',
         description: 'Just another text editor!',
         Start_url: './',
